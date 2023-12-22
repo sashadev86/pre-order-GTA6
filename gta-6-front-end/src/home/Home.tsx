a18593d9bb76e587c160bef7ecac13415eabb6f4
Aliaksandr <djvalentinoff@gmail.com>
2023-12-22 11:03:02 +0300

    complete task

--- gta-6-front-end/src/home/Home.tsx
+++ gta-6-front-end/src/home/Home.tsx
@@ -1,44 +1,64 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import styles from "./Home.module.css";
-
-const isSuccess = false;
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import styles from './Home.module.css'
 
 interface IFormState {
-  name: string;
-  email: string;
+	name: string
+	email: string
 }
 
 function Home() {
-  const { register, handleSubmit } = useForm<IFormState>();
+	const { register, handleSubmit, reset } = useForm<IFormState>()
+	const [isLoading, setIsLoading] = useState(false)
+	const [isSuccess, setIsSuccess] = useState(false)
 
-  const onSubmit: SubmitHandler<IFormState> = (data) => {
-    // console.log(data);
-  };
+	const onSubmit: SubmitHandler<IFormState> = data => {
+		setIsLoading(true)
+		fetch('http://localhost:5000/api', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then(response => response.json())
+			.then(data => {
+				if (!data) return
 
-  return (
-    <div className={styles.wrapper}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {isSuccess ? (
-          <div className={styles.success}>Форма отправлена</div>
-        ) : (
-          <>
-            <h1>GTA 6 - Оставь заявку</h1>
-            <input
-              type="name"
-              placeholder="Введите имя:"
-              {...register("name")}
-            />
-            <input
-              type="email"
-              placeholder="Введите Email:"
-              {...register("email")}
-            />
-            <button>Хочу ГТА!</button>
-          </>
-        )}
-      </form>
-    </div>
-  );
+				setIsSuccess(true)
+				reset()
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{isSuccess ? (
+					<div className={styles.success}>Форма отправлена!</div>
+				) : (
+					<>
+						<h1>GTA 6 - Оставь заявку</h1>
+						<input
+							type='name'
+							placeholder='Введите имя:'
+							{...register('name')}
+						/>
+						<input
+							type='email'
+							placeholder='Введите Email:'
+							{...register('email')}
+						/>
+						<button disabled={isLoading}>
+							{isLoading ? 'Загрузка...' : 'Хочу ГТА!'}
+						</button>
+					</>
+				)}
+			</form>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
